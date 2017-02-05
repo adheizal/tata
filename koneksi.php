48a084c4e559f66e8851b83262760361dc303245
adhelhgi <lhgi.fahmy@yahoo.com>
2017-02-06 06:43:18 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
-$ldap_dn = "cn=admin,dc=tata,dc=com";
-$ldap_pass = "jangkrik";
-$dn = "dc=tata,dc=com";
+$ldap_dn = "cn=admin,dc=ldap,dc=com";
+$ldap_pass = "root";
+$dn = "dc=ldap,dc=com";
 
 $ldap_con = ldap_connect("10.0.2.15",389);
 ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);

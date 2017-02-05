e983053687fb3c2b83fea56bd8ddce330067e459
adhelhgi <lhgi.fahmy@yahoo.com>
2017-02-06 06:50:56 +0700

    Update add.php

--- add.php
+++ add.php
@@ -9,17 +9,19 @@
     if ($ldap_con) {
     // bind with appropriate dn to give update access
     $r = ldap_bind($ldap_con, $ldap_dn, $ldap_pass);
-    $name = "didik";
+    $name = "user";
 
     // prepare data
     $info["cn"] = $name;
     $info["sn"] = $name;
-    $info["objectclass"] = "inetOrgPerson";
-    $info["userPassword"] = '{SHA}' . base64_encode(pack('H*',sha1("jangkrik")));;
+    $info["objectclass"][0] = "inetOrgPerson";
+    $info["objectclass"][1] = "account";
+    $info["objectclass"][2] = "shadowAccount";
+    $info["userPassword"] = '{SHA}' . base64_encode(pack('H*',sha1("root")));;
     $info["uid"] = $name;
 
     // add data to directory
-    $r = ldap_add($ldap_con, "cn=$name, cn=user-group,ou=user,dc=tata,dc=com", $info);
+    $r = ldap_add($ldap_con, "cn=$name, cn=user-group,ou=user,dc=ldap,dc=com", $info);
 
     ldap_close($ldap_con);
 } else {

05aec0cc6dd54151b6df4e78fb01768751e74d54
adhelhgi <lhgi.fahmy@yahoo.com>
2017-02-06 06:52:43 +0700

    Update hapus.php

--- hapus.php
+++ hapus.php
@@ -7,7 +7,7 @@
 		if ($ldap_con) {
 		    // bind with appropriate dn to give update access
 		    $data = ldap_bind($ldap_con, $ldap_dn, $ldap_pass);
-		    $data = ldap_delete($ldap_con, "cn=$id, cn=user-group,ou=user,dc=tata,dc=com");
+		    $data = ldap_delete($ldap_con, "cn=$id, cn=user-group,ou=user,dc=ldap,dc=com");
 		    echo "<script>alert('BERHASIL ....');</script>";
 		    echo "<meta http-equiv='refresh' content='0; url=index.php'>";
 		}else{
